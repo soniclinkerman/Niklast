1b519a73a6c47b71d519a3cd54699720a4ad31f8
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-09-13 15:06:41 -0400

    [NIK-4] Created Display Input component

---
+++ src/ToDo/DisplayInput/display-input.component.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Button from '../../Button/button.component';
+import UserInput from "../UserInput/user-input.component"
+
+function DisplayInput({display}){
+  return (
+    <div>
+        {display.map(input => {
+            return(
+            <h3>{input}</h3>
+            )
+            })}
+    </div>
+    );
+}
+
+export default DisplayInput
