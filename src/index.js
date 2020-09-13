73e51f1db467ef378d80c63e431d6d406549ad6a
Abaraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2020-09-13 21:35:54 +0100

    [TRAF-7] Replaced the context.js into context folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Provider from './context';
+import Provider from './context/context';
 
 ReactDOM.render(
   <React.StrictMode>
